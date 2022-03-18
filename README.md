91ee08de7cf158306df9f3560319a3a85313614b
Pierre Flandé <flande.pierre@gmail.com>
2022-03-18 18:47:14 +0100

    Add animations. Add positioning and use case

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ This project creates a **tooltip** using **React**.
 `yarn` or `npm i` to install packages
 `yarn dev` or `npm run dev` to run the app locally
 App starts on http://localhost:5000/
+It shows 2 examples of how to use this component:
+
+- the first one is a header using positioning defined in your jsx. 4 options are available: `top`, `bottom`, `left` and `right`.
+- another one (still WIP) shows how to use tooltip with smart positining, ie the tooltip will set its own position if it is outside of the viewport.
 
 ## Requirements
 
@@ -14,18 +18,18 @@ node >= 12.2.0
 
 ## What it does
 
-1. Display a tooltip when:
+1. Display tooltip when:
 
 - hovering an element
 - focusing an element
 
-2. Hide the tooltip when :
+2. Hide tooltip when :
 
 - mouse leaves element
 - focus leves element (ie tabs to another focusable element)
 - user hits Escape
 
-3. Provide basic styles and animation
+3. Provide basic styles and animation (using SCSS only)
 
 ## What it doesn't do
 
@@ -34,4 +38,4 @@ Use fancy libs.
 ## What it doesn't do but could pretend to
 
 User customizable animations.
-Offer more positioning options (on element 4 corners).
+Offer more positioning options (on element's 4 corners).
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,86 @@
-import "../styles/Header.scss";
+import { useState } from "react";
 import Message from "./Message";
 import TooltipWrapper from "./TooltipWrapper";
+import "../styles/Header.scss";
 
 function Header() {
+  const [position, setPosition] = useState("bottom");
+
+  const handleChange = (e) => {
+    setPosition(e.target.value);
+  };
+
   return (
-    <header>
-      <ul>
-        <TooltipWrapper position="bottom" content="🏡 is for everyone">
-          <li>Home</li>
-        </TooltipWrapper>
-        <li>Our product</li>
-        <TooltipWrapper position="bottom" content={Message}>
-          <li>Who are we?</li>
-        </TooltipWrapper>
-        <li>Jobs</li>
-        <TooltipWrapper
-          position="bottom"
-          content="Some content with veryveryverylong words inside"
-        >
-          <li>FAQ</li>
-        </TooltipWrapper>
-      </ul>
-    </header>
+    <>
+      <article>
+        <p>
+          This header component demonstrates use of a controlled tooltip: 4
+          positions are available, passed as parameters to the tooltip
+          component.
+        </p>
+        <p>Go ahead and play with the position!</p>
+        <p>
+          As you'll see, this version does not guarantee tooltip visibility in
+          viewport.
+        </p>
+      </article>
+      <form>
+        <input
+          type="radio"
+          id="top"
+          value="top"
+          name="position"
+          onChange={handleChange}
+        />
+        <label htmlFor="top">⬆️</label>
+
+        <input
+          type="radio"
+          id="bottom"
+          value="bottom"
+          name="position"
+          onChange={handleChange}
+        />
+        <label htmlFor="bottom">⬇️</label>
+
+        <input
+          type="radio"
+          id="left"
+          value="left"
+          name="position"
+          onChange={handleChange}
+        />
+        <label htmlFor="left">⬅️</label>
+
+        <input
+          type="radio"
+          id="right"
+          value="right"
+          name="position"
+          onChange={handleChange}
+        />
+        <label htmlFor="right">➡️</label>
+        <p>Position: {position}</p>
+      </form>
+      <header>
+        <ul>
+          <TooltipWrapper position={position} content="🏡 is for everyone">
+            <li>Home</li>
+          </TooltipWrapper>
+          <li>Our product</li>
+          <TooltipWrapper position={position} content={Message}>
+            <li>Who are we?</li>
+          </TooltipWrapper>
+          <li>Jobs</li>
+          <TooltipWrapper
+            position={position}
+            content="Some content with veryveryverylong words inside"
+          >
+            <li>FAQ</li>
+          </TooltipWrapper>
+        </ul>
+      </header>
+    </>
   );
 }
 
